4a011dac357ec0bc404f1625333f103714a5f3bb
Ryan Northfield <ryannorthfield@yahoo.com>
2018-09-15 20:26:43 -0500

    cleaning up

--- client/webaudio.js
+++ client/webaudio.js
@@ -1,4 +1,151 @@
+const vrr = require('verse-reference-regex');
+var final_transcript = '';
+var recognizing = false;
+var ignore_onend;
+var start_timestamp;    
+var recognition;
+function recognize() {
+    //-------------------------BEGIN Speech Recognition-------------------------//
+    if (!('webkitSpeechRecognition' in window)) {
+        upgrade();
+    } else {
+        //start_button.style.display = 'inline-block';
+        recognition = new webkitSpeechRecognition();
+        recognition.continuous = true;
+        recognition.interimResults = true;
     
+        recognition.onstart = function() {
+        //lower input area's text is stored in inputText var
+        //inputText = document.getElementById("inputArea").value.toLowerCase();
+        
+        
+        recognizing = true;
+        showInfo('info_speak_now');
+        };
+    
+        recognition.onerror = function(event) {
+        if (event.error == 'no-speech') {
+            showInfo('info_no_speech');
+            ignore_onend = true;
+        }
+        if (event.error == 'audio-capture') {
+            showInfo('info_no_microphone');
+            ignore_onend = true;
+        }
+        if (event.error == 'not-allowed') {
+            if (event.timeStamp - start_timestamp < 100) {
+            showInfo('info_blocked');
+            } else {
+            showInfo('info_denied');
+            }
+            ignore_onend = true;
+        }
+        };
+    
+        recognition.onend = function() {
+        recognizing = false;
+        if (ignore_onend) {
+            return;
+        }
+        if (!final_transcript) {
+            showInfo('info_start');
+            return;
+        }
+        showInfo('');
+        if (window.getSelection) {
+            window.getSelection().removeAllRanges();
+            var range = document.createRange();
+            range.selectNode(document.getElementById('final_span'));
+            window.getSelection().addRange(range);
+        }
+        };
+    
+        recognition.onresult = function(event) {
+        var interim_transcript = '';
+        if (typeof(event.results) == 'undefined') {
+            recognition.onend = null;
+            recognition.stop();
+            upgrade();
+            return;
+        }
+        
+        var transcript = '';
+        for (var i = event.resultIndex; i < event.results.length; ++i) {
+            if (event.results[i].isFinal) {
+            final_transcript = event.results[i][0].transcript;
+            transcript = event.results[i][0].transcript.toLowerCase();
+            } else {
+            transcript = event.results[i][0].transcript.toLowerCase();
+            }
+        }
+        
+        const verseRequiringRegex = vrr.createRegex();
+        const verseReference = final_transcript.match(verseRequiringRegex);
+        if (verseReference) {
+        displayVerse(verseReference);
+        }
+        };
+    
+    }
+
+}
+  
+  function displayVerse(verseReference) {
+    let verse2 = {verseID: 1, chapterID: 1, bookID: 'Genesis', versionID: 'ESV', action: 'new'};
+  
+    let inputVerse = verseReference[0];
+    let split = inputVerse.split(/(?:\s|:)/);
+    let _verseID = split.pop();
+    let _chapterID = split.pop();
+    let _bookID = split.join('');
+    let _versionID = 'esv';
+    let verse1 = {verseID: _verseID, chapterID: _chapterID, bookID: _bookID, versionID: _versionID, action: 'new'};
+  
+    $.post( "/", verse1, function( data ) {
+      $('#verse').html(data.text);
+    });
+  }
+  //-------------------------END Speech Recognition-------------------------//
+  
+  function showInfo(s) {
+    // if (s) {
+    //   for (var child = info.firstChild; child; child = child.nextSibling) {
+    //     if (child.style) {
+    //       child.style.display = child.id == s ? 'inline' : 'none';
+    //     }
+    //   }
+    //   info.style.visibility = 'visible';
+    // } else {
+    //   info.style.visibility = 'hidden';
+    // }
+  }
+  //-------------------------END Recognition Processing-------------------------//
+  
+  function upgrade() {
+    start_button.style.visibility = 'hidden';
+    showInfo('info_upgrade');
+  
+    if (start_button.style.visibility == 'false') {
+      startButton(true);
+    }
+  }
+  
+  function startButton(event) {
+    if (recognizing) {
+      $('.fa-microphone').removeClass('recording');
+      recognition.stop();
+      return;
+    }
+    $('.fa-microphone').addClass('recording');
+    final_transcript = '';
+    recognition.lang = 6;
+    recognition.start();
+    ignore_onend = false;
+    showInfo('info_allow');
+    start_timestamp = event.timeStamp;
+  }
+
+
 // //-------------------------BEGIN Languages-------------------------//
 // var langs =
 // [['Afrikaans',       ['af-ZA']],
@@ -83,3 +230,9 @@
 //   select_dialect.style.visibility = list[1].length == 1 ? 'hidden' : 'visible';
 // }
 // //-------------------------END Languages-------------------------//
+
+
+module.exports = {
+    startButton,
+    recognize
+};
